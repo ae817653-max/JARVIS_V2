16e350f21caaaef9b42a5e0bd7381400deb3b345
ae817653-max <ae817653@gmail.com>
2026-08-14 08:23:00 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from kivy.app import App
 from kivy.uix.label import Label
 class JarvisApp(App):
     def build(self):
-        return Label(text="JARVIS V2 OK")
+        return Label(text="JARVIS FIX SDKMANAGER")
 JarvisApp().run()
